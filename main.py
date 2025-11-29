fa211b1c7fb95c16f12ba3818952bb801c6e9f03
Antonia Gilles <195089796+AntoniaSophie04@users.noreply.github.com>
2025-11-29 15:49:13 +0100

    some minor changes

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from Game.GameSetup import GameSetup
 from Game.GameHandler import GameHandler
 
+# initializes and runs a GameSetup
 setup = GameSetup()
 config = setup.run() # config will store the player's choices in a map
 
+# GameSetup returns the two players
 p1 = config[0]
 p2 = config[1]
 
+# initializes and runs a GameHandler that will start the game
 game=GameHandler(player1=p1, player2=p2)
 game.play()
